b93f14eed63c4cee903336060c16914fe485846c
4ika64ik <162592298+4ika64ik@users.noreply.github.com>
2024-05-26 21:33:36 +0300

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -14,7 +14,7 @@ export default function App() {
 
     // Инициализация пикселя Facebook
     ReactPixel.init('388751950664047', options);
-    ReactPixel.init('1094581028510610', options);
+    ReactPixel.init('1151163746085504', options);
 
     // Отслеживание события PageView при каждом изменении маршрута
     useEffect(() => {
